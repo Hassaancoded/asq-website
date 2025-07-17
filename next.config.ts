f0c060e5e429f593d7f321867152767ac3ff62df
Muhammad Hassaan <hassaanmohammad722@gmail.com>
2025-07-17 19:38:37 +0500

    Switch to Vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
- 
+  // No basePath, no assetPrefix for Vercel
   images: {
-    unoptimized: true // Required for static export with images
+    unoptimized: true // Optional for now, safe to keep
   }
 };
 
